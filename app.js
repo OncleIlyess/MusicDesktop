c27679a1b70f0807f1a8f412f76e65e246816e1a
unknown <pro.agentgeek88@gmail.com>
2021-05-31 00:10:17 +0200

    Upload de la source

---
+++ app.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path')
+const {pathToFileURL} = require('url');
+function createWindow () {
+      const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        resizable: true,
+        webPreferences: {
+          contextIsolation: true
+        },
+        icon: path.join(__dirname, 'assets/img/music.png')
+      })
+win.loadURL('https://music.lumzapp.com');
+    }
+app.whenReady().then(createWindow)
+app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
